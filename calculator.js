c06a94162b60bb85dfec44d15d3c271a6be44044
Ezrovat <danic97@gmail.com>
2025-09-17 01:02:49 +0200

    add basic operation logic and ui backbone

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let a;
+let b;
+let operator;
+
+function operate(operation, a, b) {
+    return operation(a,b);
+}
+
+function sum(a, b) {
+    return a+b;
+}
+
+function substract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+
+
